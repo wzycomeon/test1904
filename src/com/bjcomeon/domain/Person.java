b2e6a1b804558526741aa324ad5ddd5acab029ed
Wangziyuan <wzy@bjcomeon.com>
2021-04-13 23:16:44 +0800

    3.0

--- src/com/bjcomeon/domain/Person.java
+++ src/com/bjcomeon/domain/Person.java
@@ -3,6 +3,7 @@ package com.bjcomeon.domain;
 public class Person {
     private String id;
     private String name;
-    private String loc;
+    private String location;
     private String pro;
+
 }
